d33a80da7aad1f0fef5647ff116f2d8b65c2eb86
AnhHa <hmanh.fit@gmail.com>
2018-05-07 18:26:35 +0700

    refs #2 Update Splash screen

---
+++ app/src/main/java/hamaianh/online/com/activities/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package hamaianh.online.com.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import hamaianh.online.com.R;
+
+/**
+ * Created by Ha Anh on 5/7/2018.
+ */
+
+public class SplashScreenActivity extends Activity{
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_lay);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
